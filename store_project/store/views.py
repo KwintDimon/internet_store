098c463b4beef95d6c535394879dbb0f720e7429
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2024-04-13 16:17:06 +0300

    create migration

--- store_project/store/views.py
+++ store_project/store/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 
+def build_template(lst: list, cols: int) -> list[list]:
+    return [lst[i:i + cols] for i in range(0, len(lst), cols)]
 
 
 def product_list(request):
-    return render(request, 'store/product_list.html', context={'product_title':'Range Rover'})
+    products = [
+        {'title': 'Range Rover', 'info': 'Lorem ipsum...', 'price':15000},
+        {'title': 'Land Rover Defender', 'info': 'Lorem ipsum...', 'price':100000},
+        {'title': 'Range Rover Sport', 'info': 'Lorem ipsum...', 'price':250000},
+
+        {'title': 'Range Rover', 'info': 'Lorem ipsum...', 'price':15000},
+        {'title': 'Land Rover Defender', 'info': 'Lorem ipsum...', 'price':100000},
+        {'title': 'Range Rover Sport', 'info': 'Lorem ipsum...', 'price':250000},
+
+        {'title': 'Range Rover', 'info': 'Lorem ipsum...', 'price':15000},
+        {'title': 'Land Rover Defender', 'info': 'Lorem ipsum...', 'price':100000},
+        {'title': 'Range Rover Sport', 'info': 'Lorem ipsum...', 'price':250000},
+    ]
+    return render(
+        request,
+        'store/product_list.html',
+        context={'product_list':build_template(products, 3)}
+    )
