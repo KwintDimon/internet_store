584518087b272dab4d1072feeea40af94df3ee25
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2024-04-17 00:11:35 +0300

    Create superuser

--- store_project/store/models.py
+++ store_project/store/models.py
@@ -5,3 +5,7 @@ class Product(models.Model):
     title = models.CharField(max_length=150, db_index=True)
     info = models.TextField(blank=True)
     price = models.IntegerField()
+
+
+    def __str__(self) -> str:
+        return self.title
--- store_project/store/views.py
+++ store_project/store/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
+from.models import Product
 
 def build_template(lst: list, cols: int) -> list[list]:
     return [lst[i:i + cols] for i in range(0, len(lst), cols)]
 
 
 def product_list(request):
-    products = [
-        {'title': 'Range Rover', 'info': 'Lorem ipsum...', 'price':15000},
-        {'title': 'Land Rover Defender', 'info': 'Lorem ipsum...', 'price':100000},
-        {'title': 'Range Rover Sport', 'info': 'Lorem ipsum...', 'price':250000},
-
-        {'title': 'Range Rover', 'info': 'Lorem ipsum...', 'price':15000},
-        {'title': 'Land Rover Defender', 'info': 'Lorem ipsum...', 'price':100000},
-        {'title': 'Range Rover Sport', 'info': 'Lorem ipsum...', 'price':250000},
-
-        {'title': 'Range Rover', 'info': 'Lorem ipsum...', 'price':15000},
-        {'title': 'Land Rover Defender', 'info': 'Lorem ipsum...', 'price':100000},
-        {'title': 'Range Rover Sport', 'info': 'Lorem ipsum...', 'price':250000},
-    ]
+    products = Product.objects.all()
     return render(
         request,
         'store/product_list.html',
