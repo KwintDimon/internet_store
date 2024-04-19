9417c2b64d32a0d86ab4913c91b5235fcc8658de
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2024-04-19 23:12:42 +0300

    create def product_detail

--- store_project/store/views.py
+++ store_project/store/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
-from.models import Product
+from.models import *
 
 def build_template(lst: list, cols: int) -> list[list]:
     return [lst[i:i + cols] for i in range(0, len(lst), cols)]
 
 
 def product_list(request):
+    categories = Category.objects.all()
     products = Product.objects.all()
     return render(
         request,
         'store/product_list.html',
-        context={'product_list':build_template(products, 3)}
+        context={
+            'product_list':build_template(products, 3),
+            'categories': categories
+            }
     )
+
+def product_detail(request):
+        categories = Category.objects.all()
+        product = Product.objects.get(pk=1)
