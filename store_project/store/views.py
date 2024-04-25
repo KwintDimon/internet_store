e2518426167a1de3bc42790ab8e977cb92ac1725
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2024-04-25 22:48:45 +0300

    Work whith form

--- store_project/store/views.py
+++ store_project/store/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from.models import *
+from django.db.models import Q
 
 def build_template(lst: list, cols: int) -> list[list]:
     return [lst[i:i + cols] for i in range(0, len(lst), cols)]
 
 
 def product_list(request):
-    categories = Category.objects.all()
-    products = Product.objects.all()
+    categories = Category.objects.all()    
+    search_query = request.GET.get('search', None)
+    if search_query:
+        products = Product.objects.filter(
+            Q(title__icontains=search_query)
+            |
+            Q(info__icontains=search_query)
+        )
     return render(
         request,
         'store/product_list.html',
